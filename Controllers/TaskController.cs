f823d15a3bef1d962e5f34880893eb4c7ad4e249
manasavemuri19 <51434149+manasavemuri19@users.noreply.github.com>
2025-05-13 17:08:32 +0530

    Add project files.

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagerProLite.Models;
+using System.Collections.Generic;
+
+namespace TaskManagerProLite.Controllers
+{
+    public class TaskController : Controller
+    {
+        static List<TaskItem> tasks = new List<TaskItem>();
+
+        public IActionResult Index()
+        {
+            return View(tasks);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TaskItem task)
+        {
+            task.Id = tasks.Count + 1;
+            tasks.Add(task);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/TaskItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagerProLite.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
